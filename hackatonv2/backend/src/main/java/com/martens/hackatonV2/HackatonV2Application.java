03018309db8204224504cd41fcc396958cdd9ba6
Cozmin Alexandru Mlesnita <cozmin.mlesnita@gmail.com>
2022-04-15 09:57:33 +0300

    Add files via upload
    Initial commit

---
+++ hackatonv2/backend/src/main/java/com/martens/hackatonV2/HackatonV2Application.java
@@ -0,0 +1,13 @@
+package com.martens.hackatonV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonV2Application.class, args);
+	}
+
+}
